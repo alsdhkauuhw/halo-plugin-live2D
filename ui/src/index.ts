ce2e174d3c72fe8785b7730106b09451c5309408
leiye <vbblszh0338735@163.com>
2025-12-08 00:44:03 +0800

    feat：删除旧页面

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,48 +1,7 @@
 import { definePlugin } from '@halo-dev/console-shared'
-import HomeView from './views/HomeView.vue'
-import Live2DView from './views/Live2DView.vue'
-import { IconPlug } from '@halo-dev/components'
-import { markRaw } from 'vue'
 
 export default definePlugin({
   components: {},
-  routes: [
-    {
-      parentName: 'Root',
-      route: {
-        path: '/live2d',
-        name: 'Live2D',
-        component: Live2DView,
-        meta: {
-          title: 'Live2D',
-          searchable: true,
-          menu: {
-            name: 'Live2D',
-            group: '插件',
-            icon: markRaw(IconPlug),
-            priority: 0,
-          },
-        },
-      },
-    },
-    {
-      parentName: 'Root',
-      route: {
-        path: '/example',
-        name: 'Example',
-        component: HomeView,
-        meta: {
-          title: '示例页面',
-          searchable: true,
-          menu: {
-            name: '示例页面',
-            group: '示例分组',
-            icon: markRaw(IconPlug),
-            priority: 0,
-          },
-        },
-      },
-    },
-  ],
+  routes: [],
   extensionPoints: {},
 })
